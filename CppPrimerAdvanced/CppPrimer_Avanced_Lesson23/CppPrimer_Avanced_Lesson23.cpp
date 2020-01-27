8f8488b360bece0a42530a88595bb291189ac1ab
weiminxu <weiminxu1982@gmail.com>
2020-01-27 20:47:49 +1300

    template for function and class
    template for function and class

--- CppPrimerAdvanced/CppPrimer_Avanced_Lesson23/CppPrimer_Avanced_Lesson23.cpp
+++ CppPrimerAdvanced/CppPrimer_Avanced_Lesson23/CppPrimer_Avanced_Lesson23.cpp
@@ -5,18 +5,33 @@ using namespace std;
 struct absInt
 {
 public:
+	//overload operator:function call operator
 	int operator()(int val)
 	{
-		
+		return val < 0 ? -val : val;
 	}
 
 };
 
+template<typename elementType>
+struct DisplayElement
+{
+	void operator() (const elementType& element) const
+	{
+		cout << element << '';
+	}
+};
 
 int main()
 {
+	{
+		int i = -42;
+		absInt absObj;
+		unsigned int ui = absObj(i);
+		cout << ui << endl;
+	}
+	
 
-
-
+	system("pause");
 	return 0;
 }
